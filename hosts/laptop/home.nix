4de5b5ba5548edd07402330e16dc7be2c10b1f06
hacky <jon.nguyen7@protonmail.com>
2026-02-16 22:46:39 -0600

    laptop config added

---
+++ hosts/laptop/home.nix
@@ -0,0 +1,106 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../system/ssh.nix
+  ];
+
+  home = {
+    username = "hacky";
+    homeDirectory = "/home/hacky";
+    stateVersion = "25.11";
+    enableNixpkgsReleaseCheck = false;
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  dconf = {
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user.name = "hacky";
+        user.email = "jon.nguyen7@protonmail.com";
+      };
+    };
+    home-manager.enable = true;
+  };
+
+  services = {
+    gnome-keyring.enable = true;
+  };
+
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
+    accent = "peach";
+  };
+
+  home.packages = with pkgs; [
+    ## Secrets
+    gcr
+
+    ## Main
+    pandoc
+
+    ## Junk
+    # discord
+    # monero-gui
+    # obs-studio
+    qFlipper
+  ];
+
+  xdg.configFile = {
+    "btop" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/btop/";
+      recursive = true;
+    };
+    "dunst" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/dunst/";
+      recursive = true;
+    };
+    "ghostty" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/ghostty/";
+      recursive = true;
+    };
+    "helix" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/helix/";
+      recursive = true;
+    };
+    "mango" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/hosts/laptop/mango/";
+      recursive = true;
+    };
+    "nushell" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/nushell/";
+      recursive = true;
+    };
+    "rofi" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/rofi/";
+      recursive = true;
+    };
+    "waybar" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/waybar/";
+      recursive = true;
+    };
+    "hypr" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/hypr/";
+      recursive = true;
+    };
+    "foot" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/hacky/nixdots/config/foot/";
+      recursive = true;
+    };
+  };
+}
---
+++ hosts/laptop/mango/autostart.sh
@@ -0,0 +1,8 @@
+waybar &
+/run/current-system/sw/bin/soteria &
+swww-daemon &
+syncthing &
+dunst &
+NetworkManager &
+gnome-keyring-daemon --start --components=secrets &
+dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
