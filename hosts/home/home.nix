f82c0d968ac8ce9f325af1ca6ccf3f08344c3a56
hacky <jon.nguyen7@protonmail.com>
2026-03-09 12:28:00 -0500

    niri

--- hosts/home/home.nix
+++ hosts/home/home.nix
@@ -19,6 +19,13 @@
     enableNixpkgsReleaseCheck = false;
   };
 
+  gtk = {
+    enable = true;
+    colorScheme = "dark";
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+  };
+
   nixpkgs.config = {
     allowUnfree = true;
   };
