57efb24ceb6d448d5ff43c86549189f0cf54325e
hacky <jon.nguyen7@protonmail.com>
2026-03-19 12:52:30 -0500

    alacritty

---
+++ apps/alacritty.nix
@@ -0,0 +1,15 @@
+{
+  programs.alacritty = {
+    enable = true;
+
+    window = {
+      opacity = 0.6;
+      blur = true;
+    };
+
+    font = {
+      size = 16.0;
+      normal = "Iosevka";
+    };
+  };
+}
